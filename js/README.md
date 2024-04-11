a38363a720cf213280c7d86cd96b75ff464a7c6a
Martin Bartoš <mabartos@redhat.com>
2024-04-11 22:13:51 +0200

    Add Admin UI changes - authentication policies tab
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ js/README.md
@@ -0,0 +1,1 @@
+JS files for the Keycloak application
---
+++ js/apps/admin-ui/src/authentication/routes/AuthenticationPolicy.tsx
@@ -0,0 +1,26 @@
+import {lazy} from "react";
+import type {Path} from "react-router-dom";
+import {generateEncodedPath} from "../../utils/generateEncodedPath";
+import type {AppRouteObject} from "../../routes";
+
+export type AuthenticationPolicyParams = {
+    realm: string;
+    id: string;
+};
+
+const AuthenticationPolicyDetails = lazy(() => import("../AuthenticationPolicyDetails"));
+
+export const AuthenticationPolicyRoute: AppRouteObject = {
+    path: "/:realm/authentication/authn-policy/:id/",
+    element: <AuthenticationPolicyDetails/>,
+    breadcrumb: (t) => t("flowDetails"),
+    handle: {
+        access: "view-authorization",
+    },
+};
+
+export const toAuthenticationPolicy = (params: AuthenticationPolicyParams): Partial<Path> => {
+    return {
+        pathname: generateEncodedPath(AuthenticationPolicyRoute.path, params),
+    };
+};
