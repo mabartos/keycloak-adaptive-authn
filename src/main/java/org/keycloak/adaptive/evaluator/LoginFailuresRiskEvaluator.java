33f72633983e05fecd61676a256b3aa733e33ea5
Martin Bartoš <mabartos@redhat.com>
2024-04-17 00:03:00 +0200

    WiP - LoginFailuresRiskEvaluator - add logging
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- src/main/java/org/keycloak/adaptive/evaluator/LoginFailuresRiskEvaluator.java
+++ src/main/java/org/keycloak/adaptive/evaluator/LoginFailuresRiskEvaluator.java
@@ -1,5 +1,6 @@
 package org.keycloak.adaptive.evaluator;
 
+import org.jboss.logging.Logger;
 import org.keycloak.adaptive.level.Risk;
 import org.keycloak.adaptive.spi.context.RiskEvaluator;
 import org.keycloak.models.KeycloakSession;
@@ -8,6 +9,8 @@ import org.keycloak.sessions.AuthenticationSessionModel;
 import java.util.Optional;
 
 public class LoginFailuresRiskEvaluator implements RiskEvaluator {
+    private static final Logger logger = Logger.getLogger(LoginFailuresRiskEvaluator.class);
+
     private final KeycloakSession session;
     private Double risk;
 
@@ -23,14 +26,23 @@ public class LoginFailuresRiskEvaluator implements RiskEvaluator {
     @Override
     public void evaluate() {
         var realm = session.getContext().getRealm();
-        if (realm == null) return;
+        if (realm == null) {
+            logger.debug("Context realm is null");
+            return;
+        }
 
         var user = Optional.ofNullable(session.getContext().getAuthenticationSession())
                 .map(AuthenticationSessionModel::getAuthenticatedUser);
-        if (user.isEmpty()) return;
+        if (user.isEmpty()) {
+            logger.debug("Context user is null");
+            return;
+        }
 
         var loginFailures = session.loginFailures().getUserLoginFailure(realm, user.get().getId());
-        if (loginFailures == null) return;
+        if (loginFailures == null) {
+            logger.debug("Cannot obtain login failures");
+            return;
+        }
 
         // TODO compute num of failures
         // Get maximum of possible num failures - realm brute force setting
