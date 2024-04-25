b0ebb616b83c2683c75b2e0e665a75da32b190f5
Martin Bartoš <mabartos@redhat.com>
2024-04-25 23:57:41 +0200

    Add AI context message
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ src/main/java/org/keycloak/adaptive/spi/ai/AiRiskEvaluatorMessages.java
@@ -0,0 +1,15 @@
+package org.keycloak.adaptive.spi.ai;
+
+public interface AiRiskEvaluatorMessages {
+
+    // Context for AI engines with some description of the problem
+    String CONTEXT_MESSAGE = """
+            You evaluate a risk that the user trying to authenticate is fraud.
+            You return the double value in the range (0,1>, as f.e. 0.8), which means an 80% chance of the authentication attempt being very critical.
+                               
+                - Values close to 0 mean the risk of user fraud is low.
+                - Values close to 1 mean the risk of user fraud is high.
+                               
+            You need to analyze the data I provide to you and return risk values.
+            """;
+}
