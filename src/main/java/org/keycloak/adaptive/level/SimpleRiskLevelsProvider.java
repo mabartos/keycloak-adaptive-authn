9e80b35187301fb651b994c31a97547ea6cdc798
Martin Bartoš <mabartos@redhat.com>
2024-05-07 12:03:02 +0200

    Amend values for simple risk level provider
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- src/main/java/org/keycloak/adaptive/level/SimpleRiskLevelsProvider.java
+++ src/main/java/org/keycloak/adaptive/level/SimpleRiskLevelsProvider.java
@@ -6,9 +6,9 @@ import org.keycloak.adaptive.spi.level.RiskLevelsProvider;
 import java.util.List;
 
 public class SimpleRiskLevelsProvider implements RiskLevelsProvider {
-    static final RiskLevel LOW = new SimpleRiskLevel("LOW", 0.0, 0.3);
-    static final RiskLevel MEDIUM = new SimpleRiskLevel("MEDIUM", 0.3, 0.75);
-    static final RiskLevel HIGH = new SimpleRiskLevel("HIGH", 0.75, 1.0);
+    static final RiskLevel LOW = new SimpleRiskLevel("LOW", 0.0, 0.33);
+    static final RiskLevel MEDIUM = new SimpleRiskLevel("MEDIUM", 0.33, 0.66);
+    static final RiskLevel HIGH = new SimpleRiskLevel("HIGH", 0.66, 1.0);
 
     @Override
     public List<RiskLevel> getRiskLevels() {
