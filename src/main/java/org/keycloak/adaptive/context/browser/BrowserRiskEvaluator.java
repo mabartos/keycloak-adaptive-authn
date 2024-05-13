e71a95b9e040e37d7dc37b336eb74aa73bbe1cc1
Martin Bartoš <mabartos@redhat.com>
2024-05-14 00:03:35 +0200

    Add Apache license to code
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- src/main/java/org/keycloak/adaptive/context/browser/BrowserRiskEvaluator.java
+++ src/main/java/org/keycloak/adaptive/context/browser/BrowserRiskEvaluator.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.keycloak.adaptive.context.browser;
 
 import org.keycloak.adaptive.context.ContextUtils;
 import org.keycloak.adaptive.evaluator.EvaluatorUtils;
 import org.keycloak.adaptive.level.Risk;
 import org.keycloak.adaptive.spi.context.RiskEvaluator;
-import org.keycloak.adaptive.spi.context.UserContext;
 import org.keycloak.models.KeycloakSession;
 
 import java.util.Optional;
-import java.util.Set;
 
 public class BrowserRiskEvaluator implements RiskEvaluator {
     private final KeycloakSession session;
--- src/main/java/org/keycloak/adaptive/spi/level/RiskLevel.java
+++ src/main/java/org/keycloak/adaptive/spi/level/RiskLevel.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.keycloak.adaptive.spi.level;
 
 public interface RiskLevel {
