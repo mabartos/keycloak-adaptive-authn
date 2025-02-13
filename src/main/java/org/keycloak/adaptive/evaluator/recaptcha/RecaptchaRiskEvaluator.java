3cd9f34a298f0a36916440c48448b74d442f0817
Martin Bartoš <mabartos@redhat.com>
2025-02-13 21:45:09 +0100

    WIP - reCAPTCHAv3
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ src/main/java/org/keycloak/adaptive/evaluator/recaptcha/RecaptchaRiskEvaluator.java
@@ -0,0 +1,124 @@
+package org.keycloak.adaptive.evaluator.recaptcha;
+
+import org.keycloak.Config;
+import org.keycloak.adaptive.spi.evaluator.AbstractRiskEvaluator;
+import org.keycloak.authentication.FormAction;
+import org.keycloak.authentication.FormActionFactory;
+import org.keycloak.authentication.FormContext;
+import org.keycloak.authentication.ValidationContext;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+import java.util.Optional;
+
+// TODO WIP
+public class RecaptchaRiskEvaluator extends AbstractRiskEvaluator implements FormAction, FormActionFactory {
+    private final KeycloakSession session;
+
+    public RecaptchaRiskEvaluator(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public KeycloakSession getSession() {
+        return session;
+    }
+
+    @Override
+    public void buildPage(FormContext context, LoginFormsProvider form) {
+
+    }
+
+    @Override
+    public void validate(ValidationContext context) {
+
+    }
+
+    @Override
+    public void success(FormContext context) {
+
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return false;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+
+    }
+
+    @Override
+    public Optional<Double> evaluate() {
+        // TODO add client side and verification
+
+        return Optional.empty();
+    }
+
+    @Override
+    public String getDisplayType() {
+        return null;
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return new AuthenticationExecutionModel.Requirement[0];
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return null;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public FormAction create(KeycloakSession session) {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+}
