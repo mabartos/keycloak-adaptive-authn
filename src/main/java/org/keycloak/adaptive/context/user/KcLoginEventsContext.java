044c6345fbc7a747f1f2d84d27c0b1ed857fa863
Martin Bartoš <mabartos@redhat.com>
2025-02-19 23:29:50 +0100

    Add Login events user context
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ src/main/java/org/keycloak/adaptive/context/user/KcLoginEventsContext.java
@@ -0,0 +1,41 @@
+package org.keycloak.adaptive.context.user;
+
+import org.keycloak.events.Event;
+import org.keycloak.events.EventStoreProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.sessions.AuthenticationSessionModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public class KcLoginEventsContext extends LoginEventsContext {
+    private final KeycloakSession session;
+
+    public KcLoginEventsContext(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public KeycloakSession getSession() {
+        return session;
+    }
+
+    @Override
+    public Optional<List<Event>> initData() {
+        var realm = session.getContext().getRealm();
+        var user = Optional.ofNullable(session.getContext().getAuthenticationSession()).map(AuthenticationSessionModel::getAuthenticatedUser);
+
+        if (user.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(session.getProvider(EventStoreProvider.class)
+                .createQuery()
+                .realm(realm.getId())
+                .user(user.get().getId())
+                .type(EventType.LOGIN)
+                .getResultStream()
+                .toList());
+    }
+}
