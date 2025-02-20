2976c43cf16e37475b6c091851c65cfd9f849a00
Martin Bartoš <mabartos@redhat.com>
2025-02-20 17:24:05 +0100

    Add risk score to the LOGIN event
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ src/main/java/org/keycloak/adaptive/events/LoginEventsEventListener.java
@@ -0,0 +1,44 @@
+package org.keycloak.adaptive.events;
+
+import io.quarkus.logging.Log;
+import org.keycloak.adaptive.spi.engine.StoredRiskProvider;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakSession;
+
+import static org.keycloak.adaptive.events.LoginEventsEventListenerFactory.RISK_SCORE_DETAIL;
+
+public class LoginEventsEventListener implements EventListenerProvider {
+    private final KeycloakSession session;
+    private final StoredRiskProvider riskProvider;
+
+    public LoginEventsEventListener(KeycloakSession session) {
+        this.session = session;
+        this.riskProvider = session.getProvider(StoredRiskProvider.class);
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        if (event.getType() != EventType.LOGIN) {
+            return;
+        }
+
+        riskProvider.getStoredRisk().ifPresent(risk -> {
+            var score = String.valueOf(risk);
+            event.getDetails().put(RISK_SCORE_DETAIL, score);
+            Log.debugf("Added risk score ('%s') to the login session", score);
+        });
+    }
+
+    @Override
+    public void onEvent(AdminEvent event, boolean includeRepresentation) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/main/java/org/keycloak/adaptive/events/LoginEventsEventListenerFactory.java
@@ -0,0 +1,37 @@
+package org.keycloak.adaptive.events;
+
+import org.keycloak.Config;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class LoginEventsEventListenerFactory implements EventListenerProviderFactory {
+    public static final String PROVIDER_ID = "login-events-adaptive-authn";
+    public static final String RISK_SCORE_DETAIL = "risk-score";
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new LoginEventsEventListener(session);
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
