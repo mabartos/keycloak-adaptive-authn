6fdae69cf31a60f91783ccb2287b169cdc850479
Martin Bartoš <mabartos@redhat.com>
2025-02-24 22:44:18 +0100

    Rename Default risk engine to Mutiny
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- src/main/java/org/keycloak/adaptive/engine/MutinyRiskEngine.java
+++ src/main/java/org/keycloak/adaptive/engine/MutinyRiskEngine.java
@@ -38,16 +38,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static org.keycloak.adaptive.engine.DefaultRiskEngineFactory.DEFAULT_EVALUATOR_RETRIES;
-import static org.keycloak.adaptive.engine.DefaultRiskEngineFactory.DEFAULT_EVALUATOR_TIMEOUT;
-import static org.keycloak.adaptive.engine.DefaultRiskEngineFactory.EVALUATOR_RETRIES_CONFIG;
-import static org.keycloak.adaptive.engine.DefaultRiskEngineFactory.EVALUATOR_TIMEOUT_CONFIG;
+import static org.keycloak.adaptive.engine.MutinyRiskEngineFactory.DEFAULT_EVALUATOR_RETRIES;
+import static org.keycloak.adaptive.engine.MutinyRiskEngineFactory.DEFAULT_EVALUATOR_TIMEOUT;
+import static org.keycloak.adaptive.engine.MutinyRiskEngineFactory.EVALUATOR_RETRIES_CONFIG;
+import static org.keycloak.adaptive.engine.MutinyRiskEngineFactory.EVALUATOR_TIMEOUT_CONFIG;
 
 /**
  * Default risk engine for the overall risk score evaluation leveraging asynchronous and parallel processing
  */
-public class DefaultRiskEngine implements RiskEngine {
-    private static final Logger logger = Logger.getLogger(DefaultRiskEngine.class);
+public class MutinyRiskEngine implements RiskEngine {
+    private static final Logger logger = Logger.getLogger(MutinyRiskEngine.class);
 
     private final KeycloakSession session;
     private final RealmModel realm;
@@ -58,7 +58,7 @@ public class DefaultRiskEngine implements RiskEngine {
 
     private Double risk;
 
-    public DefaultRiskEngine(KeycloakSession session) {
+    public MutinyRiskEngine(KeycloakSession session) {
         this.session = session;
         this.realm = session.getContext().getRealm();
         this.tracingProvider = TracingProviderUtil.getTracingProvider(session);
@@ -101,7 +101,7 @@ public class DefaultRiskEngine implements RiskEngine {
         evaluators = requiresUser ? evaluators : evaluators.runSubscriptionOn(exec);
 
         evaluators.subscribe().with(e -> KeycloakModelUtils.runJobInTransaction(session.getKeycloakSessionFactory(), session.getContext(), s -> {
-            tracingProvider.trace(DefaultRiskEngine.class, "evaluateAll", span -> {
+            tracingProvider.trace(MutinyRiskEngine.class, "evaluateAll", span -> {
                 var evaluatedRisks = Multi.createFrom()
                         .items(e.stream())
                         .onItem().transformToUniAndConcatenate(risk -> processEvaluator(risk, requiresUser, exec, retries))
