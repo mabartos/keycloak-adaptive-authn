8541c6e842ee6dd3a9fd0ac296f3b4f3cecd492b
Martin Bartoš <mabartos@redhat.com>
2025-02-26 10:58:53 +0100

    Add RiskScoreAlgorithm SPI
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

---
+++ src/main/java/org/keycloak/adaptive/spi/engine/RiskScoreAlgorithmFactory.java
@@ -0,0 +1,6 @@
+package org.keycloak.adaptive.spi.engine;
+
+import org.keycloak.provider.ProviderFactory;
+
+public interface RiskScoreAlgorithmFactory extends ProviderFactory<RiskScoreAlgorithm> {
+}
