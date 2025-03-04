b7b4b2318922901bd699cc9905a4679cec92141d
Martin Bartoš <mabartos@redhat.com>
2025-03-04 10:51:30 +0100

    WiP - scheduled tasks after restart
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- src/main/java/org/keycloak/adaptive/engine/LoginEventsEventListenerFactory.java
+++ src/main/java/org/keycloak/adaptive/engine/LoginEventsEventListenerFactory.java
@@ -5,10 +5,15 @@ import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.utils.PostMigrationEvent;
+import org.keycloak.provider.ProviderEvent;
+import org.keycloak.provider.ProviderEventListener;
+import org.keycloak.timer.TimerProvider;
 
-public class LoginEventsEventListenerFactory implements EventListenerProviderFactory {
+public class LoginEventsEventListenerFactory implements EventListenerProviderFactory, ProviderEventListener {
     public static final String PROVIDER_ID = "login-events-adaptive-authn";
     public static final String RISK_SCORE_DETAIL = "risk-score";
+    private Runnable onClose;
 
     @Override
     public EventListenerProvider create(KeycloakSession session) {
@@ -26,12 +31,31 @@ public class LoginEventsEventListenerFactory implements EventListenerProviderFac
     }
 
     @Override
-    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
-
+    public void postInit(KeycloakSessionFactory factory) {
+        factory.register(this);
+        onClose = () -> factory.unregister(this);
     }
 
     @Override
     public void close() {
+        if (onClose != null) {
+            onClose.run();
+        }
+    }
 
+    @Override
+    public void onEvent(ProviderEvent event) {
+        if (event instanceof PostMigrationEvent pme) {
+            setupScheduledTasks(pme.getFactory());
+        }
+    }
+
+    public static void setupScheduledTasks(final KeycloakSessionFactory sessionFactory) {
+        try (KeycloakSession session = sessionFactory.create()) {
+            TimerProvider timer = session.getProvider(TimerProvider.class);
+            if (timer != null) {
+                // TODO setup scheduled continuous evaluator
+            }
+        }
     }
 }
