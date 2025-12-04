283587d24299625804738b5cd722ed12e838ec5e
Martin Bartoš <mabartos@redhat.com>
2025-12-04 18:45:49 +0100

    Basic tests
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- tests/src/test/java/io/github/mabartos/BasicAdaptiveAuthnTest.java
+++ tests/src/test/java/io/github/mabartos/BasicAdaptiveAuthnTest.java
@@ -1,27 +1,82 @@
 package io.github.mabartos;
 
-import org.hamcrest.CoreMatchers;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.realm.RealmConfig;
+import org.keycloak.testframework.realm.RealmConfigBuilder;
 import org.keycloak.testframework.server.KeycloakServerConfig;
 import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @KeycloakIntegrationTest(config = BasicAdaptiveAuthnTest.Config.class)
 public class BasicAdaptiveAuthnTest {
 
+    @InjectRealm(config = AdaptiveRealmConfig.class, ref = "adaptive")
+    ManagedRealm adaptiveRealm;
+
+    @Test
+    public void base() {
+        assertThat(adaptiveRealm, notNullValue());
+        assertThat(adaptiveRealm.getName(), is("adaptive"));
+        var representation = adaptiveRealm.getCreatedRepresentation();
+        assertThat(representation, notNullValue());
+        var browserFlow = representation.getBrowserFlow();
+        assertThat(browserFlow, is("adaptive"));
+
+        var adaptiveFlow = adaptiveRealm.admin().flows()
+                .getFlows()
+                .stream()
+                .filter(f -> f.getAlias().equals("adaptive"))
+                .findFirst()
+                .orElse(null);
+        assertThat(adaptiveFlow, notNullValue());
+        /*adaptiveFlow = adaptiveRealm.admin().flows().getFlow(adaptiveRealm.getId());
+        assertThat(adaptiveFlow, notNullValue());
+
+        var adaptiveFlowExecutions = adaptiveFlow.getAuthenticationExecutions();
+        assertThat(adaptiveFlowExecutions, notNullValue());
+        assertThat(adaptiveFlowExecutions.size() > 1, is(true));*/
+    }
+
     @Test
     public void executeServerInEmbeddedMode() throws InterruptedException {
-        assertThat(true, CoreMatchers.is(true));
         //Thread.sleep(99999999);
     }
 
     public static class Config implements KeycloakServerConfig {
-
         @Override
         public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder builder) {
             return builder.dependency("io.github.mabartos", "keycloak-adaptive-authn");
+        }
+    }
+
+    public static class AdaptiveRealmConfig implements RealmConfig {
+        public static final String REALM_JSON_NAME = "test-adaptive-realm.json";
+
+        @Override
+        public RealmConfigBuilder configure(RealmConfigBuilder realm) {
+            try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(REALM_JSON_NAME)) {
+                if (is == null) {
+                    throw new RuntimeException(REALM_JSON_NAME + " not found in classpath");
+                }
+
+                ObjectMapper mapper = new ObjectMapper();
+                RealmRepresentation realmRep = mapper.readValue(is, RealmRepresentation.class);
+
+                return RealmConfigBuilder.update(realmRep);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load " + REALM_JSON_NAME, e);
+            }
         }
     }
 }
